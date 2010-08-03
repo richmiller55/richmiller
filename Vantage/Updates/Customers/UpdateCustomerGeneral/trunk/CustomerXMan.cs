f654671e437b70408d12f5e68e3b9d38dad37b06
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2010-08-03 22:56:43 +0000

    White space indent only

--- Vantage/Updates/Customers/UpdateCustomerGeneral/trunk/CustomerXMan.cs
+++ Vantage/Updates/Customers/UpdateCustomerGeneral/trunk/CustomerXMan.cs
@@ -59,78 +59,122 @@ namespace UpdateCustomerGeneral
                 }
             }
         }
-
-    public void ChangeShipToTerr(string line)
-    {
-        string[] split = line.Split(new Char[] { '\t' });
-        string CustId = split[(int)colShipToUp.CustId];
-
-    }
-
-    public void ChangeShipToRep(string line)
-    {
-        string[] split = line.Split(new Char[] { '\t' });
-        string CustId = split[(int)colShipToUp.CustId];
-        string message = "ok";
-        bool okToUpdate = true;
-        try
+        public void ChangeShipToTerr(string line)
         {
-            custDs = this.customerObj.GetByCustID(CustId);
+            string[] split = line.Split(new Char[] { '\t' });
+            string CustId = split[(int)colShipToUp.CustId];
 
         }
-        catch (Exception e)
+        public void ChangeShipToRep(string line)
         {
-            message = e.Message;
-            message = "NoGo";
-            okToUpdate = false;
-        }
-        if (okToUpdate)
-        {
-            Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow =
-                (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
-            int custNum = custRow.CustNum;
-            string shipToNum = split[(int)colShipToUp.shipToNo];
-            string territoryId = split[(int)colShipToUp.custTerritory];
-            string salesRepCode = split[(int)colShipToUp.CustPrimeSalesRep];
-            string salesRepName = split[(int)colShipToUp.SalesRepName];
-
-            Epicor.Mfg.BO.ShipTo shipto = new Epicor.Mfg.BO.ShipTo(this.objSess.ConnectionPool);
-            Epicor.Mfg.BO.ShipToDataSet shipToDs = shipto.GetByID(custNum, shipToNum);
-            Epicor.Mfg.BO.ShipToDataSet.ShipToRow shipToRow =
-                (Epicor.Mfg.BO.ShipToDataSet.ShipToRow)shipToDs.ShipTo.Rows[0];
-            shipToRow.TerritoryID  = territoryId;
-            shipToRow.SalesRepCode = salesRepCode;
-            shipToRow.SalesRepName = salesRepName;
-            message = "OK";
+            string[] split = line.Split(new Char[] { '\t' });
+            string CustId = split[(int)colShipToUp.CustId];
+            string message = "ok";
+            bool okToUpdate = true;
             try
             {
-                shipto.Update(shipToDs);
+                custDs = this.customerObj.GetByCustID(CustId);
+
             }
             catch (Exception e)
             {
                 message = e.Message;
+                message = "NoGo";
+                okToUpdate = false;
+            }
+            if (okToUpdate)
+            {
+                Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow =
+                    (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
+                int custNum = custRow.CustNum;
+                string shipToNum = split[(int)colShipToUp.shipToNo];
+                string territoryId = split[(int)colShipToUp.custTerritory];
+                string salesRepCode = split[(int)colShipToUp.CustPrimeSalesRep];
+                string salesRepName = split[(int)colShipToUp.SalesRepName];
+
+                Epicor.Mfg.BO.ShipTo shipto = new Epicor.Mfg.BO.ShipTo(this.objSess.ConnectionPool);
+                Epicor.Mfg.BO.ShipToDataSet shipToDs = shipto.GetByID(custNum, shipToNum);
+                Epicor.Mfg.BO.ShipToDataSet.ShipToRow shipToRow =
+                    (Epicor.Mfg.BO.ShipToDataSet.ShipToRow)shipToDs.ShipTo.Rows[0];
+                shipToRow.TerritoryID  = territoryId;
+                shipToRow.SalesRepCode = salesRepCode;
+                shipToRow.SalesRepName = salesRepName;
+                message = "OK";
+                try
+                {
+                    shipto.Update(shipToDs);
+                }
+                catch (Exception e)
+                {
+                    message = e.Message;
+                }
             }
         }
-    }
-    public void ChangeTerrByID(string custId, string newTerr, string salesRepNum)
-    {
-        string message = "OK";
-        bool okToUpdate = true;
-        try
+        public void ChangeTerrByID(string custId, string newTerr, string salesRepNum)
         {
-            custDs = customerObj.GetByCustID(custId);
+            string message = "OK";
+            bool okToUpdate = true;
+            try
+            {
+                custDs = customerObj.GetByCustID(custId);
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                message = "NoGo";
+                okToUpdate = false;
+            }
+            if (okToUpdate)
+            {
+                Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
+                custRow.TerritoryID = newTerr;
+                custRow.SalesRepCode = salesRepNum;
+                try
+                {
+                    customerObj.Update(custDs);
+                }
+                catch (Exception e)
+                {
+                    message = e.Message;
+                }
+            }
         }
-        catch (Exception e)
+        public void ChangeRepOnInvoice(string custId, string repNum, string SalesRepName)
         {
-            message = e.Message;
-            message = "NoGo";
-            okToUpdate = false;
+            string message = "OK";
+            bool goOn = true;
+            try
+            {
+                custDs = customerObj.GetByCustID(custId);
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                goOn = false;
+            }
+            if (goOn)
+            {
+                Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
+                custRow.SalesRepCode = repNum;
+                custRow.SalesRepName = SalesRepName;
+                try
+                {
+                    customerObj.Update(custDs);
+                }
+                catch (Exception e)
+                {
+                    message = e.Message;
+                }
+            }
         }
-        if (okToUpdate)
+        public void ChangeTerr(int custNum, string newTerr)
         {
+            string message = "OK";
+            custDs = customerObj.GetByID(custNum);
             Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
             custRow.TerritoryID = newTerr;
-            custRow.SalesRepCode = salesRepNum;
+            custRow.SalesRepCode = "43";
+
             try
             {
                 customerObj.Update(custDs);
@@ -140,25 +184,19 @@ namespace UpdateCustomerGeneral
                 message = e.Message;
             }
         }
-    }
-    public void ChangeRepOnInvoice(string custId, string repNum, string SalesRepName)
-    {
-        string message = "OK";
-        bool goOn = true;
-        try
+        public void setData(int custNum)
         {
-            custDs = customerObj.GetByCustID(custId);
-        }
-        catch (Exception e)
-        {
-            message = e.Message;
-            goOn = false;
-        }
-        if (goOn)
-        {
+            string message = "OK";
+            custDs = customerObj.GetByID(custNum);
             Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
-            custRow.SalesRepCode = repNum;
-            custRow.SalesRepName = SalesRepName;
+            custRow.PrintStatements = true;
+            string currentVia = custRow.ShipViaCode;
+            int result = currentVia.CompareTo("");
+            if (result == 0)
+            {
+                custRow.ShipViaCode = "FGRB";
+            }
+
             try
             {
                 customerObj.Update(custDs);
@@ -168,116 +206,75 @@ namespace UpdateCustomerGeneral
                 message = e.Message;
             }
         }
-    }
-    public void ChangeTerr(int custNum, string newTerr)
-    {
-        string message = "OK";
-        custDs = customerObj.GetByID(custNum);
-        Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
-        custRow.TerritoryID = newTerr;
-        custRow.SalesRepCode = "43";
-
-        try
+        public void setCustGrp(string custId,string newGrp)
         {
-            customerObj.Update(custDs);
+            string message = "OK";
+            bool okToUpdate = true;
+            try
+            {
+                custDs = customerObj.GetByCustID(custId);
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                message = "NoGo";
+                okToUpdate = false;
+            }
+            if (okToUpdate)
+            {
+                Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
+                custRow.GroupCode = newGrp;
+                try
+                {
+                    customerObj.Update(custDs);
+                }
+                catch (Exception e)
+                {
+                    message = e.Message;
+                }
+            }
         }
-        catch (Exception e)
+        public void setTerrRep(string custId,string terr,string salesRep,
+			                   bool changeT,bool changeR,bool changeLock)
         {
-            message = e.Message;
-        }
-    }
-    public void setData(int custNum)
-    {
-        string message = "OK";
-        custDs = customerObj.GetByID(custNum);
-        Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
-        custRow.PrintStatements = true;
-        string currentVia = custRow.ShipViaCode;
-        int result = currentVia.CompareTo("");
-        if (result == 0)
-        {
-            custRow.ShipViaCode = "FGRB";
-        }
-
-        try
-        {
-            customerObj.Update(custDs);
-        }
-        catch (Exception e)
-        {
-            message = e.Message;
-        }
-    }
-    public void setCustGrp(string custId,string newGrp)
-    {
-        string message = "OK";
-        bool okToUpdate = true;
-        try
-        {
-            custDs = customerObj.GetByCustID(custId);
-        }
-        catch (Exception e)
-        {
-            message = e.Message;
-            message = "NoGo";
-            okToUpdate = false;
-        }
-        if (okToUpdate)
-        {
+            string message = "OK";
+            try
+            {
+                custDs = customerObj.GetByCustID(custId);
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+            }
+             
             Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
-            custRow.GroupCode = newGrp;
-            try
+            bool dirty = false;
+            if (changeT)
             {
-                customerObj.Update(custDs);
+                custRow.TerritoryID = terr;
+                dirty = true;
             }
-            catch (Exception e)
+            if (changeR)
             {
-                message = e.Message;
+                custRow.SalesRepCode = salesRep;
+                dirty = true;
             }
-        }
-    }
-    public void setTerrRep(string custId,string terr,string salesRep,
-			               bool changeT,bool changeR,bool changeLock)
-    {
-        string message = "OK";
-        try
-        {
-            custDs = customerObj.GetByCustID(custId);
-        }
-        catch (Exception e)
-        {
-            message = e.Message;
-        }
-         
-        Epicor.Mfg.BO.CustomerDataSet.CustomerRow custRow = (Epicor.Mfg.BO.CustomerDataSet.CustomerRow)custDs.Customer.Rows[0];
-        bool dirty = false;
-        if (changeT)
-        {
-            custRow.TerritoryID = terr;
-            dirty = true;
-        }
-        if (changeR)
-        {
-            custRow.SalesRepCode = salesRep;
-            dirty = true;
-        }
-        if (changeLock)
-        {
-            custRow.TerritoryLock = true;
-            dirty = true;
-        }
-        if (dirty)
-        {
-            try
+            if (changeLock)
             {
-                customerObj.Update(custDs);
+                custRow.TerritoryLock = true;
+                dirty = true;
             }
-            catch (Exception e)
+            if (dirty)
             {
-                message = e.Message;
+                try
+                {
+                    customerObj.Update(custDs);
+                }
+                catch (Exception e)
+                {
+                    message = e.Message;
+                }
             }
         }
     }
-
-}
 }
