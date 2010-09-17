764a59bd574f4559f3f2659484ba028eb0f13b45
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2010-09-17 23:44:27 +0000

    printing using windows forms for now. hope it is fast

---
+++ Vantage/InvBox/trunk/InvoiceFormater.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace InvBox
+{
+    public class InvoiceFormater
+    {
+        public ArrayList lines = new ArrayList();
+        Invoice i;
+
+
+        public InvoiceFormater(Invoice inv)
+        {
+		this.i = inv;
+        }
+        void Header()
+        {
+            lines.Add("--------------------------------------------------------+");
+            lines.Add("|                                                       |");
+            lines.Add("  Invoice " + i.InvoiceNo.ToString();
+    }
+}
---
+++ Vantage/InvBox/trunk/PrtForm.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InvBox
+{
+    public partial class PrtForm : Form
+    {
+        public PrtForm()
+        {
+            InitializeComponent();
+        }
+        public void printIt()
+        {
+            string testStr = "Here we go array list of lines";
+            try
+            {
+                Font printFont = new Font("Arial", 10);
+                printDocument1.PrintPage += new PrintPageEventHandler(this.pd_PrintPage);
+                printDocument1.Print();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void pd_PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            float linesPerPage = 0;
+            float yPos = 0;
+            int count = 0;
+            float leftMargin = ev.MarginBounds.Left;
+            float topMargin = ev.MarginBounds.Top;
+            string line = null;
+
+            // Calculate the number of lines per page.
+            linesPerPage = ev.MarginBounds.Height / printFont.GetHeight(ev.Graphics);
+
+            // Print each line of the file.
+            while (count < linesPerPage && ((line = streamToPrint.ReadLine()) != null))
+            {
+                yPos = topMargin + (count *
+                   printFont.GetHeight(ev.Graphics));
+                ev.Graphics.DrawString(line, printFont, Brushes.Black,
+                   leftMargin, yPos, new StringFormat());
+                count++;
+            }
+
+            // If more lines exist, print another page.
+            if (line != null)
+                ev.HasMorePages = true;
+            else
+                ev.HasMorePages = false;
+        }
+
+        private void InitializeComponent()
+        {
+        }
+    }
+}
+
+
+
