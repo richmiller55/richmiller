3f04f2969a28f0a688cb6c6d7fc847e00fcb9bb6
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2010-09-21 02:24:18 +0000

    filled out StreetAddress, still need to add a one line City State Zip method

--- Vantage/InvBox/trunk/StreetAddress.cs
+++ Vantage/InvBox/trunk/StreetAddress.cs
@@ -6,7 +6,72 @@ namespace InvBox
 {
     public class StreetAddress
     {
-    private string address1 = new String();
-    public StreetAddress()
-{
+
+        private string custID = new String();
+        private string custName = new String();
+        private string address1 = new String();
+        private string city = new String();
+        private string state = new String();
+        private string zipCode = new String();
+        private string country = new String();
+        public StreetAddress()
+        {
+
+        }
+        public string CustID
+        {
+            get
+            {
+                return custID;
+            }
+            set
+            {
+                custID = value;
+            }
+        }
+        public string Address1
+        {
+            get
+            {
+                return address1;
+            }
+            set
+            {
+                address1 = value;
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+            }
+        }
+        public string ZipCode
+        {
+            get
+            {
+                return zipCode;
+            }
+            set
+            {
+                zipCode = value;
+            }
+        }
+    }
 }
