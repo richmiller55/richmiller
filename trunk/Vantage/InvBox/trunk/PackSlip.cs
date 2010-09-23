8748c7e248077ca7cd19b19477f959633ad8ce1c
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2010-09-23 18:07:01 +0000

    more conversions today

--- trunk/Vantage/InvBox/trunk/PackSlip.cs
+++ trunk/Vantage/InvBox/trunk/PackSlip.cs
@@ -10,6 +10,8 @@ namespace InvBox
         Epicor.Mfg.Core.Session session;
         Epicor.Mfg.BO.CustShip custShipObj;
         Epicor.Mfg.BO.CustShipDataSet custShipDs;
+        Epicor.Mfg.BO.CustShipDataSet.ShipDtlRow dtlRow;
+        Epicor.Mfg.BO.CustShipDataSet.ShipHeadRow custShipRow;
         bool invoiced;
         int packNum;
         int CustNum;
@@ -23,7 +25,7 @@ namespace InvBox
         string customerTerms;
         bool customerFF;
         string newInvoices = string.Empty;
-        public PackSlipInfo(Epicor.Mfg.Core.Session vanSession, int pack)
+        public PackSlip(Epicor.Mfg.Core.Session vanSession, int pack)
         {
             packFound = false;
             packNum = pack;
@@ -40,6 +42,10 @@ namespace InvBox
                 GetCustomerInfo();
             }
         }
+        public void ExtactAddress(StreetAddress sa)
+        {
+            sa.Address1 = this.custShipRow.BillAddr;
+        }
         void InitCustShip()
         {
             custShipObj = new Epicor.Mfg.BO.CustShip(session.ConnectionPool);
@@ -48,9 +54,10 @@ namespace InvBox
             try
             {
                 custShipDs = custShipObj.GetByID(packNum);
-                Epicor.Mfg.BO.CustShipDataSet.ShipHeadRow custShipRow;
-                custShipRow = (Epicor.Mfg.BO.CustShipDataSet.ShipHeadRow)custShipDs.ShipHead.Rows[0];
+
+                this.custShipRow = (Epicor.Mfg.BO.CustShipDataSet.ShipHeadRow)custShipDs.ShipHead.Rows[0];
                 CustNum = custShipRow.CustNum;
+
                 BuyGroup bgCheck = new BuyGroup(session, CustNum);
                 this.IsBuyGroup = bgCheck.GetBuyGroupMember();
                 string trackingNum = custShipRow.TrackingNumber;
@@ -59,11 +66,11 @@ namespace InvBox
                 {
                     packNeedsTracking = false;
                 }
-                Epicor.Mfg.BO.CustShipDataSet.ShipDtlRow dtlRow;
+
                 dtlRow = (Epicor.Mfg.BO.CustShipDataSet.ShipDtlRow)custShipDs.ShipDtl.Rows[0];
                 this.OrderNum = dtlRow.OrderNum;
                 invoiced = custShipRow.Invoiced;
-                custShipDs.Dispose();
+                // custShipDs.Dispose();
             }
             catch (Exception e)
             {
@@ -74,91 +81,6 @@ namespace InvBox
             if (result)
             {
                 packFound = true;
-            }
-        }
-        private void PostDetailTrackingNumbers(string[] trackingSplit,Shipment ship)
-        {
-            Epicor.Mfg.BO.TrackingDtl trackDtlObj = new Epicor.Mfg.BO.TrackingDtl(session.ConnectionPool);
-            string blank = "";
-            foreach (string trackNumber in trackingSplit)
-            {
-
-                if (trackNumber.Equals(blank)) // .Equals(blank,StringComparison.Ordinal))
-                {
-                    continue;
-                }
-                Epicor.Mfg.BO.TrackingDtlDataSet ds = new
-                    Epicor.Mfg.BO.TrackingDtlDataSet();
-                trackDtlObj.GetNewTrackingDtl(ds, this.OrderNum);
-                Epicor.Mfg.BO.TrackingDtlDataSet.TrackingDtlRow row =
-                    (Epicor.Mfg.BO.TrackingDtlDataSet.TrackingDtlRow)ds.TrackingDtl.Rows[0];
-                row.OrderNum = this.OrderNum;
-                row.CaseNum = "";
-                row.Company = "CA";
-                row.PackNum = this.PackNum;
-                row.TrackingNumber = trackNumber;
-                Hashtable hashWeights = ship.GetWeights();
-                row.Weight = (decimal)hashWeights[trackNumber];
-                row.ShipmentType = "CUST";
-                string message = "OK";
-
-                try
-                {
-                    
-                    trackDtlObj.Update(ds);
-                }
-                catch (Exception e)
-                {
-                    // tracking no did not post
-                    message = e.Message;
-                }
-            }
-        }
-        public void PostTrackingToPack(string trackingStr, decimal weight, Shipment ship)
-        {
-            bool result = true;
-            string message;
-            try
-            {
-                Epicor.Mfg.BO.CustShipDataSet.ShipHeadRow custShipRow;
-                custShipRow = (Epicor.Mfg.BO.CustShipDataSet.ShipHeadRow)custShipDs.ShipHead.Rows[0];
-                custShipRow.Weight = weight;
-                string[] trackingSplit = trackingStr.Split(':');
-                this.PostDetailTrackingNumbers(trackingSplit, ship);
-
-                string tracking = trackingSplit[0];
-                if (tracking.Length > 50)
-                {
-                    custShipRow.TrackingNumber = tracking.Substring(0, 49);
-                }
-                else
-                {
-                    custShipRow.TrackingNumber = tracking;
-                }
-                if (trackingStr.Length > 1000)
-                {
-                    custShipRow.Character01 = trackingStr.Substring(0, 999);
-                }
-                else
-                {
-                    custShipRow.Character01 = trackingStr;
-                }
-                try
-                {
-                    custShipObj.Update(custShipDs);
-                }
-                catch (Exception e)
-                {
-                    // tracking no did not post
-                    message = e.Message;
-                    result = false;
-                }
-            }
-            catch (Exception e)
-            {
-                // tracking no did not post
-                message = e.Message;
-                result = false;
             }
         }
         void GetOrderInfo()
