4d56a57887921fc5597cf479f4817a08ff6d030c
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2011-01-03 06:39:55 +0000

    checkpoint code, still does not void the tag

--- Vantage/Updates/PhysInventory/VoidTags/trunk/VoidTagXman.cs
+++ Vantage/Updates/PhysInventory/VoidTags/trunk/VoidTagXman.cs
@@ -2,7 +2,9 @@ using System;
 using System.IO;
 using System.Data.SqlClient;
 using System.Text.RegularExpressions;
+using System.Text;
 using Epicor.Mfg.BO;
+
 namespace VoidTags
 {
     public class VoidTagXman
@@ -24,6 +26,39 @@ namespace VoidTags
             this.tag = new CountTag(objSess.ConnectionPool);
         }
         public void VoidTag(string line)
+        {
+            string[] split = line.Split(new Char[] { '\t' });
+            string tagNumber = split[(int)tagList.tagNum];
+            string partNumber = split[(int)tagList.partNum];
+            StringBuilder query = new StringBuilder(1000);
+            query.Append(@"Company = 'CA' And PartNum = '" + partNumber + "\'");
+            query.Append(@" And WareHouseCode = '01' And BinNum = '10011'");
+            query.Append(@" And DimCode = '' And LotNum = ''");
+            query.Append(@" And ( SysDate > '12/31/2010' Or (SysDate = '12/31/2010' And SysTime >= '51690'))");
+            query.Append(@" BY PartNum ");
+            PartTran partTran = new PartTran(objSess.ConnectionPool);
+            bool morePages;
+            PartTranListDataSet partList = partTran.GetList(query.ToString(), 0, 0, out morePages);
+            StringBuilder tagQuery = new StringBuilder(1000);
+            tagQuery.Append("Company = 'CA' And GroupID = 'J11'");
+            tagQuery.Append(" And TagNum <> \'" + tagNumber + "\' And PartNum = '1129'");
+            tagQuery.Append(" And WareHouseCode = '01' And BinNum = '10011'");
+            tagQuery.Append(" And TagNum <> '' BY TagNum ");
+
+            CountTagListDataSet countTagList = tag.GetList(tagQuery.ToString(), 0, 0, out morePages);
+            CountTagDataSet ds = tag.GetByID("J11", tagNumber);
+            try
+            {
+                tag.DeleteByID("J11", tagNumber);
+                // tag.VoidTag("J11", tagNumber, ds);
+            }
+            catch (Exception e)
+            {
+                string message = e.Message;
+            }
+        }
+            
+        public void VoidTagOld(string line)
         {
             string[] split = line.Split(new Char[] { '\t' });
             string tagNumber = split[(int)tagList.tagNum];
