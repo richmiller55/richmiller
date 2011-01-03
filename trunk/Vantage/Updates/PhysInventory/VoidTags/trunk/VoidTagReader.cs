c0548acd67ee6e26ac333b0df0bd5c862fc24b0f
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2011-01-03 00:01:32 +0000

    laid out and compiles, does not void the tag yet

---
+++ trunk/Vantage/Updates/PhysInventory/VoidTags/trunk/VoidTagReader.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+
+namespace VoidTags
+{
+    public enum tagList
+    {
+        tagNum
+    }
+    class VoidTagReader
+    {
+        string file = "D:/users/rich/data/VoidTags/VoidTagsList010311.txt";
+
+        StreamReader tr;
+        public VoidTagReader()
+        {
+            tr = new StreamReader(file);
+            processFile();
+        }
+        void processFile()
+        {
+            string line = "";
+            VoidTagXman xman = new VoidTagXman();
+
+            while ((line = tr.ReadLine()) != null)
+            {
+                xman.VoidTag(line);
+            }
+        }
+    }
+}
+
+
+
