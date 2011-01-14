880b6ce5711b5626cf50b40aca307dc4f424e319
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2011-01-14 19:06:42 +0000

    import a shell

---
+++ Vantage/Updates/Orders/OrderCloseLines/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PartUpdate
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            UpdateTextReader reader = new UpdateTextReader();
+            Application.Run(new Form1());
+
+        }
+    }
+}
