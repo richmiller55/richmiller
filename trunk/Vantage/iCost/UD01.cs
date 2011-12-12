bbc92a716f5424ad1944aa73eb72ec0363fc7eec
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2011-12-12 23:00:41 +0000

    the style object will have all the calculation either built in. Resulting values serierlize to UD01

--- trunk/Vantage/iCost/UD01.cs
+++ trunk/Vantage/iCost/UD01.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Epicor.Mfg.BO;
+using Epicor.Mfg.Core;
 namespace iCost
 {
     class CostTable
@@ -9,20 +10,34 @@ namespace iCost
         Epicor.Mfg.Core.Session objSess;
         public CostTable()
         {
-            objSess = new Epicor.Mfg.Core.Session("rich", "homefed55",
-                "AppServerDC://VantageDB1:8331", Epicor.Mfg.Core.Session.LicenseType.Default);
+            objSess = new Session("rich", "homefed55",
+                "AppServerDC://VantageDB1:8331", Session.LicenseType.Default);
+            this.GetOldRecord();
+        }
+        public void AddNewRecord()
+        {
             UD01 ud01Obj = new UD01(objSess.ConnectionPool);
             UD01DataSet ud01Ds = new UD01DataSet();
             ud01Obj.GetaNewUD01(ud01Ds);
-            // ud01Obj.GetNewUD01(ud01Ds, "1200", "", "", "");
-            // UD01DataSet.UD01Row row = (UD01DataSet.UD01Row)
             UD01DataSet.UD01Row row = (UD01DataSet.UD01Row)ud01Ds.UD01.Rows[0];
-            row.Character01 = "Rich Miller";
+            row.Character01 = "Rich Miller III";
             row.Key1 = "1200";
             try
             {
-                
                 ud01Obj.Update(ud01Ds);
+            }
+            catch (Exception e)
+            {
+                string message = e.Message;
+            }
+        }
+        public void GetOldRecord()
+        {
+            UD01 ud01Obj = new UD01(objSess.ConnectionPool);
+            try
+            {
+                UD01DataSet oldDS = ud01Obj.GetByID("1200","","","","");
+                UD01DataSet.UD01Row row = (UD01DataSet.UD01Row)oldDS.UD01.Rows[0];           
             }
             catch (Exception e)
             {
