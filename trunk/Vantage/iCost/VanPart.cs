4f547e87172608a9675a76beb3d586996e3ec701
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2011-12-26 19:18:20 +0000

    added a VanPart, a information loaded part object managed by a PartInfo object, a collection of VanParts

---
+++ trunk/Vantage/iCost/VanPart.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iCost
+{
+    class VanPart
+    {          
+        string partNum;       
+        string partDescription;
+	    string loc;
+	    string prodCode;
+        string compRetail;
+        decimal unitPrice;
+        decimal dutyRate;
+    	decimal burden;
+        public VanPart(string partNum)
+        {
+            this.partNum = partNum;
+            unitPrice = 0M;
+	        partDescription = "";
+            compRetail = "";
+	        prodCode = "";
+        }
+        public string PartNum
+        {
+            get { return partNum; }
+            set { partNum = value; }
+        }
+        public string PartDescription
+        {
+            get { return partDescription; }
+            set { partDescription = value; }
+        }
+        public string ProdCode
+        {
+            get { return prodCode; }
+            set { prodCode = value; }
+        }
+        public string Loc
+        {
+            get { return loc; }
+            set { loc = value; }
+        }
+        public string CompRetail
+        {
+            get { return compRetail; }
+            set { compRetail = value; }
+        }
+        public decimal UnitPrice
+        {
+            get { return decimal.Round(unitPrice,2); }
+            set { unitPrice = value; }
+        }
+        public decimal DutyRate
+        {
+            get { return decimal.Round(dutyRate,2); }
+            set { dutyRate = value; }
+        }
+        public decimal Burden
+        {
+            get { return decimal.Round(burden,2); }
+            set { burden = value; }
+        }
+    }
+}
