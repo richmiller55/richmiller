2b1e34ee69f973f1c0f5e71909089b2b602f6996
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2012-03-26 04:50:58 +0000

    added and hooked up a xml reader for walmart addresses

--- trunk/Vantage/Updates/Customers/ShipToAdds/Program.cs
+++ trunk/Vantage/Updates/Customers/ShipToAdds/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ShipToLoad
 {
@@ -11,9 +12,31 @@ namespace ShipToLoad
         [STAThread]
         static void Main()
         {
- //           Application.EnableVisualStyles();
- //           Application.SetCompatibleTextRenderingDefault(false);
-            DataReader reader = new DataReader();
-         }
+            //           Application.EnableVisualStyles();
+            //           Application.SetCompatibleTextRenderingDefault(false);
+            // DataReader reader = new DataReader();
+            ProcessXmlDir();
+
+        }
+
+        static void ProcessXmlDir()
+        {
+            string dir = "I:/edi/shipto/xml/";
+            DirectoryInfo mainDir = new DirectoryInfo(dir);
+            try
+            {
+                FileSystemInfo[] ediShipTos = mainDir.GetFileSystemInfos();
+                foreach (FileSystemInfo shipToFile in ediShipTos)
+                {
+                    string fileName = shipToFile.Name;
+                    XmlReader reader = new XmlReader(dir, fileName);
+                    reader.runIt();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
     }
 }
