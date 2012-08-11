e834ae695955bd2d928a39532c44d859a3a7b146
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2012-08-11 02:36:20 +0000

    I am parseing the json, need some data structures, and sort of a object building stratigy

--- trunk/Vantage/Updates/POfeed/CouchReader.cs
+++ trunk/Vantage/Updates/POfeed/CouchReader.cs
@@ -1,13 +1,89 @@
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Text;
 using System.Xml;
 using System.IO;
 
 using SharpCouch;
+using LitJson;
 
 namespace POfeed
 {
+    class VooRec
+    {
+      public string Customer;
+      public string LineDesc;
+      public Dictionary<string, int> CustomerShipDate;
+      public Dictionary<string, int> PromiseDt;
+      public Dictionary<string, int> RequestedShipDate;
+      public Dictionary<string, int> ProformaDeliveryDate;
+      public Dictionary<string, int> ExAsiaDate;
+      public Dictionary<string, int> DueDate;
+      public int POLine;
+      public string IUM;
+      public int RelQty;
+      public int PORelNum;
+      public int PONum;
+      public int OpenLine;
+      public float UnitCost;
+      public string PartNum;
+      public string WarehouseCode;
+      public string poid;
+      public string VendorName;
+      public int ContainerID;
+      public VooRec(string json)
+        {
+            Dictionary<string,int> CustomerShipDate = new Dictionary<string,int>();
+            Dictionary<string,int> PromiseDt = new Dictionary<string,int>();
+            Dictionary<string, int> RequestedShipDate = new Dictionary<string, int>();
+            Dictionary<string, int> ProformaDeliveryDate = new Dictionary<string, int>();
+            Dictionary<string, int> ExAsiaDate = new Dictionary<string, int>();
+            Dictionary<string, int> DueDate = new Dictionary<string, int>();
+        }
+    }
+    class VooDoc
+    {
+        public VooDoc(string jsondoc)
+        {
+            Dictionary<string, VooRec> doc = new Dictionary<string, VooRec>();
+        }
+ 
+    }
+    class DateUnit
+    {
+        string POID;
+        int current;
+        int vantage;
+        public DateUnit()
+        {
+            current = 0;
+            vantage = 0;
+        }
+        public int Current
+        {
+            get
+            {
+                return current;
+            }
+            set
+            {
+                current = value;
+            }
+        }
+        public int Vantage
+        {
+            get
+            {
+                return vantage;
+            }
+            set
+            {
+                vantage = value;
+            }
+        }
+    }
+
     class CouchReader
     {
     	private string mServer;
@@ -20,11 +96,25 @@ namespace POfeed
             string db = "chain";
             string docID = "VooDoc";
             string doc = mCouchWrap.GetDocument(server, db, docID);
-            
-        }
-    }
-}
 
+            JsonReader reader = new JsonReader(doc);
+            while (reader.Read())
+            {
+                if (reader.Token.Equals("PropertyName"))
+                {
+                    switch (reader.Value)
+                    {
+                        case "ProformaDeliveryDate":
+
+                Console.WriteLine(reader.Token + "  " + reader.Value);
+            }
+            Console.WriteLine("i am done");
+            // VooDoc mydoc = LitJson.JsonMapper.ToObject<VooDoc>(doc);
+            
+            
+            // Object a = reader.Read();
+        }
+            
 
 
 
