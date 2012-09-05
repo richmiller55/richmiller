9740a18942a801fa27aecee417942a64753398ca
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2012-09-05 23:44:39 +0000

    removed some comments

--- Vantage/Updates/Orders/ChangeOrders/Program.cs
+++ Vantage/Updates/Orders/ChangeOrders/Program.cs
@@ -7,9 +7,6 @@ namespace ChangeOrders
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
