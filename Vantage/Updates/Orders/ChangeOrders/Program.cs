275d34694882ccf9667c1cb5726a8bd15e256375
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2012-09-05 23:41:45 +0000

    added the two files

---
+++ Vantage/Updates/Orders/ChangeOrders/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+using System.IO;
+
+namespace ChangeOrders
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+          RunFile();
+        }
+        static void RunFile()
+        {
+            string file = "I:/data/updates/orders/Prop65Cutover.txt";
+            StreamReader tr;
+            tr = new StreamReader(file);
+            UpdateSalesOrder xman = new UpdateSalesOrder(); 
+            string line = "";
+            while ((line = tr.ReadLine()) != null)
+            {
+                xman.ProcessOrder(line);
+            }
+        }
+    }
+}
