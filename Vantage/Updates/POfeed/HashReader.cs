b60143998501335249d6f3f805fa5159afdf3bda
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2013-05-08 01:20:17 +0000

    dont change the file name every time

--- Vantage/Updates/POfeed/HashReader.cs
+++ Vantage/Updates/POfeed/HashReader.cs
@@ -28,7 +28,7 @@ namespace POfeed
         }
         public void RunFile()
         {
-            string file = "I:/data/updates/po/poUpdates_30Apr2013.txt";
+            string file = "I:/data/updates/po/poUpdates.txt";
             StreamReader tr = new StreamReader(file);
             string line = "";
             POXman xman = new POXman();
