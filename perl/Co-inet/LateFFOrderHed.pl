bfc4636f17c5cac1d76a186b173424aefa14d39a
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2013-05-17 23:58:25 +0000

    Late just pulls the order header jobs pulls the two jobs tables

---
+++ perl/Co-inet/LateFFOrderHed.pl
@@ -0,0 +1,10 @@
+use strict;
+
+use lib "../perlLib";
+use Extract::OutToFile;
+use Extract::Coinet::OrderHed;
+
+sub main {
+    Extract::Coinet::OrderHed->new();
+}
+main();
--- perl/Co-inet/jobs.pl
+++ perl/Co-inet/jobs.pl
@@ -3,9 +3,11 @@ use strict;
 use lib "../perlLib";
 use Extract::OutToFile;
 use Extract::Coinet::JobHead;
+use Extract::Coinet::JobOpDtl;
 
 sub main {
     Extract::Coinet::JobHead->new();
+    Extract::Coinet::JobOpDtl->new();
 }
 
 
