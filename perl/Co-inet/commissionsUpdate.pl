fb0858cca46b41686330abf521aa864c110e180a
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2014-05-03 21:10:51 +0000

    new container thing, removed extra junk from commissionsUpdate

--- perl/Co-inet/commissionsUpdate.pl
+++ perl/Co-inet/commissionsUpdate.pl
@@ -38,37 +38,10 @@ use Extract::Coinet::Fiscal;
 main();
 
 sub main {
-#    Extract::Coinet::SalesDetail->new();
     Extract::Coinet::Customer->new();
-#    Extract::Coinet::InvcHead->new();
-#    Extract::Coinet::InvcDtl->new();
-#    Extract::Coinet::InvcMisc->new();
-#    Extract::Coinet::PartBin->new();
-#    Extract::Coinet::Part->new();
-#    Extract::Coinet::OrderDtl->new();
-#    Extract::Coinet::OrderHed->new();
-#    Extract::Coinet::ProdGrup->new();
     Extract::Coinet::CustGrup->new();
     Extract::Coinet::SalesCat->new();
-#    Extract::Coinet::ShipHead->new();
-#    Extract::Coinet::ShipDtl->new();
-#    Extract::Coinet::PartWhse->new();
-#    Extract::Coinet::CustBillTo->new();
-#    Extract::Coinet::PORel->new();
-#    Extract::Coinet::ContainerHeader->new();
-#    Extract::Coinet::ContainerDetail->new();
-#    Extract::Coinet::PODetail->new();
-#    Extract::Coinet::POHeader->new();
-#    Extract::Coinet::ShipTo->new();
-#    Extract::Coinet::InvcTax->new();
     Extract::Coinet::SalesTer->new();
     Extract::Coinet::SalesRep->new();
     Extract::Coinet::Region->new();    
-#    Extract::Coinet::CustXPrt->new();            
-#    Extract::Coinet::PartCost->new();
-#    Extract::Coinet::PLGrupBrk->new();
-#    Extract::Coinet::PLPartBrk->new();
-#    Extract::Coinet::RMADtl->new();
-#    Extract::Coinet::PartMtl->new();
-#    Extract::Coinet::Fiscal->new();            
 }
