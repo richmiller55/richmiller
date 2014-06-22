050ebc9a49302de9c8bb1a5fce57ed1b7b9b07f8
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2014-06-22 05:01:58 +0000

    doubt if this works, will fix sunday

--- costUpdate/Xman.cs
+++ costUpdate/Xman.cs
@@ -40,6 +40,7 @@ namespace costUpdate
         public string dataSet;
         Epicor.Mfg.Core.Session objSess;
         Epicor.Mfg.BO.CostAdjustment costAdjustment;
+        Epicor.Mfg.BO.Part partObj;
 
         //   pilot 8331
         //   test 8321  verify
@@ -47,8 +48,34 @@ namespace costUpdate
         public CostXman()
         {
             this.objSess = new Epicor.Mfg.Core.Session("rich", "homefed55",
-                "AppServerDC://VantageDB1:8331", Epicor.Mfg.Core.Session.LicenseType.Default);
+                "AppServerDC://VantageDB1:8321", Epicor.Mfg.Core.Session.LicenseType.Default);
             this.costAdjustment = new Epicor.Mfg.BO.CostAdjustment(objSess.ConnectionPool);
+            this.partObj = new Epicor.Mfg.BO.Part(objSess.ConnectionPool);
+        }
+
+        public void updateCostMethod(string line)
+        {
+            string[] split = line.Split(new Char[] { '\t' });
+            string partNum = split[(int)rowLayout.UPC];
+            if (partNum.Equals("UPC")) return;
+            if (partNum.Equals("Part Number")) return;
+            if (partNum.Equals("UPC Number")) return;
+
+            if (this.partObj.PartExists(partNum))
+            {
+                ds = this.partObj.GetByID(partNum);
+                Epicor.Mfg.BO.PartDataSet.PartRow row;
+                row = (Epicor.Mfg.BO.PartDataSet.PartRow)ds.Part.Rows[0];
+                row.CostMethod = "S";
+                try
+                {
+                    this.partObj.Update(ds);
+                }
+                catch (Exception e)
+                {
+                    string message = e.Message;
+                }
+            }
         }
         public void UpdateCost(CostDetail cost)
         {
@@ -60,8 +87,10 @@ namespace costUpdate
             Epicor.Mfg.BO.CostAdjustmentDataSet.CostAdjustmentRow row =
                 (Epicor.Mfg.BO.CostAdjustmentDataSet.CostAdjustmentRow)ds.CostAdjustment.Rows[0];
             row.ReasonCode = "COST";
-            row.ReasonCodeDesc = "Cost Adjust";
+            row.ReasonCodeDesc = "Std Cost Setup 21-Jun-14";
             row.Reference = "rlm-test";
+            row.StdMtlUnitCost = cost.Cost;
+            row.StdBurUnitCost = 0.0m;
             row.AvgMtlUnitCost = cost.Cost;
             this.costAdjustment.PreSetCostAdjustment(ds, out requiresUserInput);
             if (requiresUserInput)
