adb3a716e89a90385ae55ec0f27abb8f2f12bd8e
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2014-06-23 04:27:29 +0000

    running code now,

--- costUpdate/TextReader.cs
+++ costUpdate/TextReader.cs
@@ -21,56 +21,20 @@ namespace costUpdate
         public UpdateTextReader()
         {
             this.tr = new StreamReader(filedir + fName);
-            
+            ProcessFile();
+
         }
         void ProcessFile()
         {
+            String line;
+            CostXman xman = new CostXman();
             while ((line = this.tr.ReadLine()) != null)
             {
-
-                AddStdCost(line);
-                SetPartToStandardCost(line);
-            }
-        }
-        void AddStdCost(string line)
-        {
-                    string[] split = line.Split(new Char[] { '\t' });
-                string partNum = split[(int)rowLayout.UPC];
-                string newStdCostStr = split[(int)rowLayout.cost];
-            CostXman xman = new CostXman();
-
-                CostDetail detail = new CostDetail(partNum);
-                string sCost = split[(int)rowLayout.cost];
-                bool allOK = true;
-                try {
-                    detail.Cost = System.Convert.ToDecimal(sCost);
-                }
-                catch (System.OverflowException){
-                    allOK = false;
-                    System.Console.WriteLine(
-                       "The conversion from string to decimal overflowed.");
-                }
-                catch (System.FormatException)
-                {
-                    allOK = false;
-                    System.Console.WriteLine(
-                        "The string is not formatted as a decimal.");
-                }
-                catch (System.ArgumentNullException)
-                {
-                    allOK = false;
-                    System.Console.WriteLine(
-                        "The string is null.");
-                }
-                if (allOK)
-                {
-                    xman.UpdateCost(detail);
-                }
+                // xman.updateStdCost(line);
+                xman.updateCostMethod(line);
             }
         }
     }
 }
-
-
 
 
