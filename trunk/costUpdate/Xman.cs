60938d0f4b8e6a70dcc3b1b1e411834d694771cc
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2014-06-27 01:13:10 +0000

    lots of save the bal bins on hand in process

--- trunk/costUpdate/Xman.cs
+++ trunk/costUpdate/Xman.cs
@@ -2,9 +2,25 @@ using System;
 using System.IO;
 using System.Data.SqlClient;
 using System.Text.RegularExpressions;
+using System.Collections;
+using Epicor.Mfg.BO;
 
 namespace costUpdate
 {
+    public class Part
+    {
+        string partNum;
+        decimal onHandQty;
+        Hashtable ht;
+        public Part(string PartNum)
+        {
+            {this.partNum = PartNum};
+        }
+        public void AddRow(string binNum, decimal onHandQty)
+        {
+            ht.Add(binNum, onHandQty);
+        }
+    }
     public class CostDetail
     {
         string partNum;
@@ -41,6 +57,7 @@ namespace costUpdate
         Epicor.Mfg.Core.Session objSess;
         Epicor.Mfg.BO.CostAdjustment costAdjustment;
         Epicor.Mfg.BO.Part partObj;
+        Epicor.Mfg.BO.InventoryQtyAdj invAdjustObj;
 
         //   pilot 8331
         //   test 8321  verify
@@ -52,7 +69,24 @@ namespace costUpdate
             this.costAdjustment = new Epicor.Mfg.BO.CostAdjustment(objSess.ConnectionPool);
             this.partObj = new Epicor.Mfg.BO.Part(objSess.ConnectionPool);
         }
-
+        public BinOnHand GetBinOnHandFromRow(InventoryQtyAdjBrwDataSet.InventoryQtyAdjBrwRow row)
+        {
+            return new BinOnHand(row.BinNum, row.OnHandQty);
+        }
+        public void GetOnHandForPart(string partNum)
+        {
+            if (this.partObj.PartExists(partNum))
+            {
+                Epicor.Mfg.BO.InventoryQtyAdjBrwDataSet ds;
+                string primarybin = "";
+                ds = this.invAdjustObj.GetInventoryQtyAdjBrw(partNum, "01", out primarybin);
+                Epicor.Mfg.BO.InventoryQtyAdjBrwDataSet.InventoryQtyAdjBrwRow row =
+                    (Epicor.Mfg.BO.InventoryQtyAdjBrwDataSet.InventoryQtyAdjBrwRow)ds.InventoryQtyAdjBrw.Rows[0];
+                // Epicor.Mfg.BO.PartDataSet ds = this.partObj.GetByID(partNum);
+                // Epicor.Mfg.BO.PartDataSet.PartRow row;
+                // row = (Epicor.Mfg.BO.PartDataSet.PartRow)ds.Part.Rows[0];
+            }
+        }
         public void updateCostMethod(string line)
         {
             string[] split = line.Split(new Char[] { '\t' });
@@ -64,7 +98,7 @@ namespace costUpdate
                 Epicor.Mfg.BO.PartDataSet.PartRow row;
                 row = (Epicor.Mfg.BO.PartDataSet.PartRow)ds.Part.Rows[0];
                 row.CostMethod = "S";
-                
+                // this.partObj.     need to call the update cost method stuff
                 try
                 {
                     this.partObj.Update(ds);
