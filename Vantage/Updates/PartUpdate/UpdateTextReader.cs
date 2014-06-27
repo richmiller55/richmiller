676cdf0480726bb4703760172db94fcb8d0f65c2
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2014-06-27 01:09:38 +0000

    added inactivate but set to do part update

--- Vantage/Updates/PartUpdate/UpdateTextReader.cs
+++ Vantage/Updates/PartUpdate/UpdateTextReader.cs
@@ -9,7 +9,7 @@ namespace PartUpdate
 {
     class UpdateTextReader
     {
-        string fName = "Inactive_Part_Update_062014.txt";
+        string fName = "MLB_Drawstring_Bag_part_update_06.25.14.txt";
         string filedir = "I:/data/updates/parts/";
         StreamReader tr;
         public UpdateTextReader()
@@ -23,11 +23,9 @@ namespace PartUpdate
             PartUpdateXman xman = new PartUpdateXman();
             while ((line = tr.ReadLine()) != null)
             {
-                /*
                 xman.PartAddUpdate(line);
                 xman.SetAsDefaultWarehouse(line);
-                 * */
-                xman.MakeInactive(line);
+                // xman.MakeInactive(line);
             }
         }
     }
