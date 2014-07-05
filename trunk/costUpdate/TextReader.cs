1e71524b0ede25785721144026f7439ce7df45b4
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2014-07-05 16:24:05 +0000

    writes a audit file

--- trunk/costUpdate/TextReader.cs
+++ trunk/costUpdate/TextReader.cs
@@ -1,3 +1,4 @@
+#undef working
 using System;
 using System.Collections.Generic;
 using System.Collections;
@@ -15,14 +16,56 @@ namespace costUpdate
     }
     class UpdateTextReader
     {
-        string fName = "stdCostUpdate.txt";
-        string filedir = "I:/data/updates/costs/";
-        StreamReader tr;
+        
         public UpdateTextReader()
         {
-            this.tr = new StreamReader(filedir + fName);
-            ProcessFile();
+            // ProcessFile();
+            StringBuilder sbReport = auditFile();
+            writeAuditReport(sbReport);
         }
+        void writeAuditReport(StringBuilder report)
+        {
+            string fileName = "autditreport.txt";
+            StreamWriter tw = new StreamWriter(fileName);
+            tw.WriteLine(report.ToString());
+            tw.Close();
+        }
+        void AddLineToReport(StringBuilder sb, PartBinRecord pbr)
+        {
+            string rl = pbr.PartNum;
+            ICollection keyColl = pbr.BinHash.Keys;
+            string whNum = "01";
+            string tab = "\t";
+            foreach (string binNum in keyColl)
+            {
+                decimal qtyOnHand = (decimal)pbr.BinHash[binNum];
+                rl = pbr.PartNum + tab + binNum + tab + qtyOnHand.ToString() + "\n";
+                sb.Append(rl);
+            }
+        }
+        StringBuilder auditFile()
+        {
+            string fName = "stdCostUpdate.txt";
+            string filedir = "I:/data/updates/costs/";
+            StreamReader tr = new StreamReader(filedir + fName);
+            
+            string line;
+            CostXman xman = new CostXman();
+            StringBuilder sbreport = new StringBuilder();
+            while ((line = tr.ReadLine()) != null)
+            {
+                // xman.updateStdCost(line);
+                string[] split = line.Split(new Char[] { '\t' });
+                string partNum = split[(int)rowLayout.UPC];
+                if (xman.IsPartInactive(partNum)) continue;
+                PartBinRecord pbr = xman.GetOnHandForPart(partNum);
+                AddLineToReport(sbreport, pbr);
+                
+            }
+            return sbreport;
+        }
+
+#if working
         void ProcessFile()
         {
             String line;
@@ -42,6 +85,7 @@ namespace costUpdate
                 xman.WriteAllBins(writeOff, partBin);
             }
         }
+#endif
     }
 }
 
