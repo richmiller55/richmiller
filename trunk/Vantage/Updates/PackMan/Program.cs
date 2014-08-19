06054b6f8fd604a5748b7f499af3102e5212d32d
richmiller55 <richmiller55@e1215e8d-e93a-c1fc-50fa-7490f15393dd>
2014-08-19 22:25:27 +0000

    add a file

---
+++ trunk/Vantage/Updates/PackMan/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PackMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PackManager mgr = new PackManager();
+        }
+    }
+}
